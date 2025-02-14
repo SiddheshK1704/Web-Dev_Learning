e8843653fb68ef965ac8f320f6bec53e74384cf5
SiddheshK1704 <siddheshkhankhoje@gmail.com>
2025-02-14 16:25:13 +0530

    Initial commit

---
+++ HTML/script.js
@@ -0,0 +1,6 @@
+// JavaScript Boilerplate
+
+// Wait for the DOM to be fully loaded before running scripts
+document.addEventListener("DOMContentLoaded", function () {
+    alert("hello world");
+});

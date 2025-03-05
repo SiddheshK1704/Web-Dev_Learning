2812e498cc67e59134f1c96a07d0a4db00481e38
SiddheshK1704 <siddheshkhankhoje@gmail.com>
2025-03-06 00:00:22 +0530

    Added TailwindCSS Learning folder

---
+++ TailwindCSS/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./dist/**/*.html"], // This will correctly match all HTML files in the dist folder and its subdirectories
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
